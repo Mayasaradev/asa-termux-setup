92dd152c3781e0c05b5bc6e430917af921f4366a
Mayasaradev <asatora588@gmail.com>
2026-07-06 23:45:02 +0700

    fix bug

--- command.sh
+++ command.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg update
 pkg upgrade -y
-pkg install git wget tree neovim nano vim python openssh nodejs termux-api gh zsh rust neofetch termux-tools lazygit ripgrep fzf fd clang ruff lua-language-server luarocks -y
+pkg install git wget tree neovim nano vim python openssh nodejs termux-api gh zsh rust neofetch termux-tools lazygit ripgrep fzf fd clang ruff lua-language-server luarocks proot-distro -y
 
 chsh -s zsh
 
@@ -41,50 +41,10 @@ neofetch --ascii_distro kali 2>/dev/null || true
 
 EOF
 
-source ~/.zshrc
+#install proot-distro debian
+if ! proot-distro list | grep -q debian; then
+  proot-distro install debian
+fi
 
 #debian
-proot-distro login debian
-apt update && apt upgrade -y
-apt install nala -y
-nala install -y python git tree vim nano wget neofetch curl tar xz-utils zsh
-ARCH=$(uname -m)
-if [ "$ARCH" = "aarch64" ]; then ARCH="arm64"; else ARCH="x86_64"; fi
-curl -LO https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-${ARCH}.tar.gz
-tar xzvf nvim-linux-${ARCH}.tar.gz
-rm -rf /opt/nvim
-mv nvim-linux-${ARCH} /opt/nvim
-ln -sf /opt/nvim/bin/nvim /usr/bin/nvim
-
-chsh -s zsh
-
-cat >~/.zshrc <<'EOF'
-clear
-autoload -Uz vcs_info
-setopt PROMPT_SUBST
-
-zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr ' +'
-zstyle ':vcs_info:git:*' unstagedstr ' *'
-zstyle ':vcs_info:git:*' formats '%F{red}-[%F{red}%b%u%c%F{red}]'
-
-precmd() { vcs_info }
-
-zstyle ':vcs_info:git:*' formats '%F{red}-[%F{red} %b%u%c%F{red}]'
-
-PROMPT=$'%F{blue}%F{red}┌──(%F{blue}%n@debian%F{red})-[%F{white}%*%F{red}]-[%F{white}%~%F{red}]${vcs_info_msg_0_}\n└─%F{blue}❯ %f'
-
-alias up="nala update && nala upgrade -y"
-alias in="nala install -y"
-alias cls='clear'
-alias rl='source ~/.zshrc'
-alias zshrc='nvim ~/.zshrc'
-alias x='exit'
-
-neofetch --ascii_distro kali 2>/dev/null || true
-
-EOF
-
-exit
-
-echo "Selesai! Restart Termux."
+proot-distro login debian -- bash -c 'curl -fsSL https://raw.githubusercontent.com/Mayasaradev/asa-termux-setup/main/command_debian.sh | bash'
---
+++ commnad_debian.sh
@@ -0,0 +1,40 @@
+apt update && apt upgrade -y
+apt install nala -y
+nala install -y python git tree vim nano wget neofetch curl tar xz-utils zsh
+
+ARCH=$(uname -m)
+if [ "$ARCH" = "aarch64" ]; then ARCH="arm64"; else ARCH="x86_64"; fi
+curl -LO https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-${ARCH}.tar.gz
+tar xzvf nvim-linux-${ARCH}.tar.gz
+rm -rf /opt/nvim
+mv nvim-linux-${ARCH} /opt/nvim
+ln -sf /opt/nvim/bin/nvim /usr/bin/nvim
+
+chsh -s zsh
+
+cat >~/.zshrc <<'EOF'
+clear
+autoload -Uz vcs_info
+setopt PROMPT_SUBST
+
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' stagedstr ' +'
+zstyle ':vcs_info:git:*' unstagedstr ' *'
+zstyle ':vcs_info:git:*' formats '%F{red}-[%F{red}%b%u%c%F{red}]'
+
+precmd() { vcs_info }
+
+zstyle ':vcs_info:git:*' formats '%F{red}-[%F{red} %b%u%c%F{red}]'
+
+PROMPT=$'%F{blue}%F{red}┌──(%F{blue}%n@debian%F{red})-[%F{white}%*%F{red}]-[%F{white}%~%F{red}]${vcs_info_msg_0_}\n└─%F{blue}❯ %f'
+
+alias up="nala update && nala upgrade -y"
+alias in="nala install -y"
+alias cls='clear'
+alias rl='source ~/.zshrc'
+alias zshrc='nvim ~/.zshrc'
+alias x='exit'
+
+neofetch --ascii_distro kali 2>/dev/null || true
+
+EOF
